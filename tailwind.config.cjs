b99e03650f968b9e7cb712a9c15bf663266a9e69
Rudolf Halas <rudolf@limedeck.io>
2023-01-08 21:31:14 +0100

    put up a minimal version for now

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        light: "#F7F7F7",
+        grey: "#ECECEC",
+        accent: "#0439D9",
+        black: "#262626",
+      },
+    },
   },
   plugins: [],
 };
